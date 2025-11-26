8da4060672fe39c923826633200c755f1c8121c4
Andini <163494512+Threeguana@users.noreply.github.com>
2025-11-26 10:27:07 +0700

    Delete directory

--- OOP1_AndiniTribuana_23Sep2025/OOP1/src/dataStructure/Calculator.java
+++
@@ -1,9 +0,0 @@
-package dataStructure;
-
-public class Calculator {
-    // method untuk menjumlahkan dua bilangan bulat
-    public int add(int a, int b) {
-        // mengembalikan hasil penjumlahan a dan b
-        return a + b;
-    }
-}
